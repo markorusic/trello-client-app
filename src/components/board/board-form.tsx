d2b5c1bef1186aa37f6634dcaba8fc62888ee003
Marko Rusic <markorusic98@gmail.com>
2021-02-14 03:45:39 +0100

    feat: board form validation (#6)

--- src/components/board/board-form.tsx
+++ src/components/board/board-form.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { FormikConfig } from 'formik'
 import { Optional } from 'utility-types'
+import { reduce, set } from 'lodash'
 import { BoardMutationDto } from '../../services/board-service'
 import {
   Form,
@@ -23,7 +24,22 @@ export const BoardForm: FC<BoardFormProps> = ({
   ...props
 }) => {
   return (
-    <Form {...props} initialValues={initialValues}>
+    <Form
+      {...props}
+      initialValues={initialValues}
+      validate={values =>
+        reduce(
+          values,
+          (result, value, key) => {
+            if (!value) {
+              set(result, key, 'validation.required')
+            }
+            return result
+          },
+          {}
+        )
+      }
+    >
       <TextInput
         name="name"
         label="commons.name"
