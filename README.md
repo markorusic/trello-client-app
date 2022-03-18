b0ae230c3967f1cdcc194905909809169c0212a9
Marko Rusic <markousp5@gmail.com>
2022-03-18 23:04:14 +0100

    update login stying & readme

--- README.md
+++ README.md
@@ -4,15 +4,13 @@
 
 Setup local env file:
 ```sh
-touch .env.local
-cat .env > .env.local
+cat .env.example > .env
 ```
 
-Now edit `.env.local`, and set your tokens:
+Now edit `.env`, and set your tokens:
 ```sh
 REACT_APP_TRELLO_API_BASE_URL=https://api.trello.com/1
 REACT_APP_TRELLO_API_KEY=********
-REACT_APP_TRELLO_SERVER_TOKEN=********
 ```
 
 Install node modules:
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -14,52 +14,68 @@ export const LoginPage = () => {
 
   return (
     <div className="flex flex-col justify-center items-center pt-16 text-center">
-      <h1 className="text-2xl">Login with your Trello account</h1>
+      <div className="w-96">
+        <h1 className="text-2xl">Login with your Trello account</h1>
 
-      <div className="w-96 my-4">
-        Get your Trello account token and enter it in form below. This data is
-        not stored or sent anywhere. When you refresh the page it will be gone.
-      </div>
-
-      <a
-        className="px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition-colors"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={`https://trello.com/1/authorize?${params}`}
-      >
-        Get your Trello account token
-      </a>
-
-      <form
-        className="pt-4"
-        onSubmit={event => {
-          event.preventDefault()
-          const token = new FormData(event.currentTarget).get('token') as string
-          auth.login(token)
-        }}
-      >
-        <div className="flex flex-col">
-          <label className="font-semibold" htmlFor="token">
-            Your Trello Token
-          </label>
-          <input
-            className="border border-gray-600 px-4 py-2 rounded-md"
-            required
-            id="token"
-            name="token"
-            type="text"
-            placeholder="Enter your token..."
-          />
+        <div className="my-4">
+          Get your Trello account token and enter it in form below. This data is
+          not stored or sent anywhere. When you refresh the page it will be
+          gone.
         </div>
-        <div className="my-2">
-          <button
-            className="px-4 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition-colors"
-            type="submit"
+
+        <a
+          className="px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://trello.com/1/authorize?${params}`}
+        >
+          Get your Trello account token
+        </a>
+
+        <form
+          className="pt-8"
+          onSubmit={event => {
+            event.preventDefault()
+            const token = new FormData(event.currentTarget).get(
+              'token'
+            ) as string
+            auth.login(token)
+          }}
+        >
+          <div className="flex flex-col">
+            <label className="font-semibold" htmlFor="token">
+              Your Trello Token
+            </label>
+            <input
+              className="border border-gray-600 px-4 py-2 rounded-md"
+              required
+              id="token"
+              name="token"
+              type="text"
+              placeholder="Enter your token..."
+            />
+          </div>
+          <div className="my-2">
+            <button
+              className="px-4 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition-colors w-full"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <div className="py-8 absolute top-2 right-2">
+          <a
+            className="text-gray-500 border border-gray-200 px-4 py-2 rounded-md"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/markorusic/trello-client-app"
           >
-            Login
-          </button>
+            GitHub - Source Code
+          </a>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
