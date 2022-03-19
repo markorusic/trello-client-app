50f1cfcf697a43a56dad6bdcdf19c9c67b2cbdb7
Marko Rusic <marko.rusic@novafabrika.rs>
2022-03-19 02:19:23 +0100

    trello popup login

--- src/components/auth-provider.tsx
+++ src/components/auth-provider.tsx
@@ -2,6 +2,54 @@ import { createContext, FC, useContext, useState } from 'react'
 import axios from 'axios'
 import { env } from '../config/env'
 
+export const trelloPopupLogin = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const params = new URLSearchParams({
+      expiration: '1day',
+      name: 'Trello Clinet App',
+      scope: 'read,write',
+      response_type: 'token',
+      callback_method: 'postMessage',
+      return_url: window.location.origin,
+      key: env.TRELLO_API_KEY
+    })
+
+    const authOrigin = 'https://trello.com'
+    const authUrl = `${authOrigin}/1/authorize?${params}`
+
+    const authWindow = window.open(
+      authUrl,
+      'trello-auth-window',
+      'location=0,status=0,width=800,height=800'
+    )
+
+    const interval = setInterval(() => {
+      if (authWindow?.closed) {
+        clearInterval(interval)
+        reject(null)
+      }
+    }, 300)
+
+    const receiveMessage = (event: MessageEvent) => {
+      if (event.origin !== authOrigin || event.source !== authWindow) {
+        return
+      }
+
+      if (event.source != null) {
+        event.source.close()
+      }
+
+      if (event.data != null && /[0-9a-f]{64}/.test(event.data)) {
+        resolve(event.data as string)
+      }
+
+      window.removeEventListener('message', receiveMessage, false)
+    }
+
+    window.addEventListener('message', receiveMessage, false)
+  })
+}
+
 const createTrelloClinet = (token: string | undefined) =>
   axios.create({
     baseURL: env.TRELLO_API_BASE_URL,
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,80 +1,32 @@
-import React from 'react'
-import cx from 'classnames'
-import { useAuth } from '../components/auth-provider'
-import { env } from '../config/env'
-import {
-  Button,
-  buttonSizes,
-  buttonVariants
-} from '../shared/components/button'
+import { trelloPopupLogin, useAuth } from '../components/auth-provider'
+import { Button } from '../shared/components/button'
 
 export const LoginPage = () => {
   const auth = useAuth()
-  // @ts-ignore
-  const params = new URLSearchParams({
-    expiration: '1day',
-    name: 'Trello Clinet App',
-    scope: 'read,write',
-    response_type: 'token',
-    key: env.TRELLO_API_KEY
-  }).toString()
 
   return (
     <div className="flex flex-col justify-center items-center pt-16 text-center">
-      <div className="w-96">
+      <div>
         <h1 className="text-2xl">Login with your Trello account</h1>
 
         <div className="my-4">
-          Get your Trello account token and enter it in form below. This data is
-          not stored or sent anywhere. When you refresh the page it will be
-          gone.
+          <p className="mb-2">
+            Your data is will not be stored or sent anywhere. When you refresh
+            the page it will be gone and you will be logged out.
+          </p>
+          <p>
+            Keep in mind that every change that you make here will reflect on
+            your data on Trello.
+          </p>
         </div>
 
-        <a
-          className={cx(buttonVariants.primary, buttonSizes.sm)}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://trello.com/1/authorize?${params}`}
-        >
-          Get your Trello account token
-        </a>
+        <div className="py-2">
+          <Button onClick={() => trelloPopupLogin().then(auth.login)}>
+            Login with Trello
+          </Button>
+        </div>
 
-        <form
-          className="pt-8"
-          onSubmit={event => {
-            event.preventDefault()
-            const token = new FormData(event.currentTarget).get(
-              'token'
-            ) as string
-            auth.login(token)
-          }}
-        >
-          <div className="flex flex-col">
-            <label className="font-semibold" htmlFor="token">
-              Your Trello Token
-            </label>
-            <input
-              className="border border-gray-600 px-4 py-2 rounded-md"
-              required
-              id="token"
-              name="token"
-              type="text"
-              placeholder="Enter your token..."
-            />
-          </div>
-          <div className="my-2">
-            <Button
-              type="submit"
-              className="w-full"
-              variant="success"
-              size="md"
-            >
-              Login
-            </Button>
-          </div>
-        </form>
-
-        <div className="py-8 absolute top-2 right-2">
+        <div className="py-8 absolute bottom-0 right-2 md:top-0">
           <a
             className="text-gray-500 border border-gray-200 px-4 py-2 rounded-md"
             target="_blank"
